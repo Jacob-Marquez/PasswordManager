f8b025604e58d88a30c3a5934f1362bc3962ad04
Jacob Marquez <jacobmarquez178@gmail.com>
2025-03-08 15:17:22 -0500

    Update password generator

--- generator.py
+++ generator.py
@@ -2,6 +2,15 @@ import secrets
 import string
 
 
-def generate_password(length=16, allowed_chars=string.ascii_letters + string.digits + string.punctuation):
-    return ''.join(secrets.choice(allowed_chars) for _ in range(length))
+def generate_password(length=16, allowed_chars=string.ascii_letters + string.digits + string.punctuation) -> bytearray:
+    
+    allowed_bytes = allowed_chars.encode("ascii", errors="replace")
+    
+    password = bytearray(length)
+    
+    for i in range(length):
+        x = secrets.randbelow(len(allowed_bytes))
+        password[i] = allowed_bytes[x]
+        
+    return password
 
